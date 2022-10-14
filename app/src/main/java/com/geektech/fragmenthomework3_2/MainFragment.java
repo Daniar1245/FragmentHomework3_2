a51b64ddf9291ebe1ec1d9c131a666c2f341f916
Daniar1245 <daniarraimkulov123@gmail.com>
2022-10-14 15:52:15 +0600

    Fragment

--- app/src/main/java/com/geektech/fragmenthomework3_2/MainFragment.java
+++ app/src/main/java/com/geektech/fragmenthomework3_2/MainFragment.java
@@ -1,5 +1,6 @@
 package com.geektech.fragmenthomework3_2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,20 +10,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
-
-
+    int count  = 0;
+    private Button btnIncrement,btnDecrement;
+    private TextView clickCount;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
